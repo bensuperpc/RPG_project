3aa0841da6640e79d1153199c6fae3ca749484b3
Bensuperpc <bensuperpc@gmail.com>
2021-11-14 13:36:22 +0100

    Clean build
    Signed-off-by: Bensuperpc <bensuperpc@gmail.com>

--- RPG/coverage.sh
+++
@@ -1,2 +0,0 @@
-gcovr -r . --exclude src/test/ --exclude src/game.cpp --exclude src/lib/utils/sfml/ --exclude src/bench/ --exclude build/CMakeFiles/ --html-details -o coverage/coverage.html
-gcovr -r . --exclude src/test/ --exclude src/game.cpp --exclude src/lib/utils/sfml/ --exclude src/bench/ --exclude build/CMakeFiles/
